8d22a9b28046ecb17eab6beb81c1c0aa5ef16514
Dima Gerasimov <karlicoss@gmail.com>
2021-04-03 20:37:14 +0100

    add initial kobo processor

--- src/bleanser/modules/kobo.py
+++ src/bleanser/modules/kobo.py
@@ -16,35 +16,24 @@ class Normaliser(SqliteNormaliser):
         # todo not sure about this?.. also makes sense to run checked for cleanup/extract?
         with self.checked(db) as conn:
             self.tables = get_tables(conn)
-        assert 'podcasts' in self.tables, self.tables
-        assert 'episodes' in self.tables, self.tables
+        def check_table(name: str) -> None:
+            assert name in self.tables, self.tables
+        check_table('content')
+        check_table('Bookmark')
+        check_table('BookAuthors')
 
     def cleanup(self, c: Connection) -> None:
-        ## often changing
-        c.execute('UPDATE episodes SET thumbnail_id=-1')
-        c.execute('UPDATE podcasts SET update_date=-1,episodesNb=-1,thumbnail_id=-1,subscribers=-1')
-        ##
-
         def drop(name: str) -> None:
             c.execute(f'DROP TABLE IF EXISTS {name}')
 
-        drop('ordered_list')  # just some random numbers, always changing
-        drop('sqlite_stat1')  # ???
-        ## changing often an likely not interesting
-        drop('blocking_services')
-        drop('ad_campaign')
-        drop('bitmaps')
-        drop('fts_virtual_episode_docsize')
-        drop('fts_virtual_episode_segments')
-        drop('fts_virtual_episode_segdir')
-        ## probably unnecessary?
-        drop('chapters')
-        drop('teams')
-        drop('topics')
-        drop('radio_search_results')
-        drop('relatedPodcasts')
-        drop('content_policy_violation')  # lol
-        ##
+        drop('content') # some cached book data? so not very interesting when it changes..
+        drop('content_keys')  # just some image meta
+        drop('volume_shortcovers')  # just some hashes
+        drop('volume_tabs')  # some hashes
+        # ## often changing
+        # c.execute('UPDATE episodes SET thumbnail_id=-1')
+        # c.execute('UPDATE podcasts SET update_date=-1,episodesNb=-1,thumbnail_id=-1,subscribers=-1')
+        # ##
 
 
 import click
@@ -53,7 +42,7 @@ import click
 @click.option('--max-workers', required=False, type=int, help='Passed down to ThreadPoolExecutore. Use 0 for serial execution')
 def main(path: Path, max_workers: Optional[int]) -> None:
     # TODO collect all sqlite mimes?
-    paths = list(sorted(path.rglob('*.db')))
+    paths = list(sorted(path.rglob('*.sqlite')))
     sqlite_process(paths, Normaliser=Normaliser, max_workers=max_workers)
 
 
