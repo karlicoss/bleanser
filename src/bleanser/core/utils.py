cae7a41ff5c5ff0450a40d044cd7e5001841cbaf
Dima Gerasimov <karlicoss@gmail.com>
2021-04-03 17:18:25 +0100

    add dummy executor to run in serial mode, more robust checks for shm/wal

--- src/bleanser/core/utils.py
+++ src/bleanser/core/utils.py
@@ -10,3 +10,31 @@ def get_tables(c: Connection) -> List[str]:
     names = [c[0] for c in cur]
     names.remove('sqlite_sequence') # hmm not sure about it
     return names
+
+
+# https://stackoverflow.com/a/10436851/706389
+from typing import Any, Optional
+from concurrent.futures import Future, Executor
+class DummyExecutor(Executor):
+    def __init__(self, max_workers: Optional[int]=1) -> None:
+        self._shutdown = False
+        self._max_workers = max_workers
+
+    def submit(self, fn, *args, **kwargs) -> Future:
+        if self._shutdown:
+            raise RuntimeError('cannot schedule new futures after shutdown')
+
+        f: Future[Any] = Future()
+        try:
+            result = fn(*args, **kwargs)
+        except KeyboardInterrupt:
+            raise
+        except BaseException as e:
+            f.set_exception(e)
+        else:
+            f.set_result(result)
+
+        return f
+
+    def shutdown(self, wait: bool=True) -> None:
+        self._shutdown = True
