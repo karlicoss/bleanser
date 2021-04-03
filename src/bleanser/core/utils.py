1924fe0d8c3e022dfc3fb7430d9604fe5a10ad86
Dima Gerasimov <karlicoss@gmail.com>
2021-04-03 20:37:14 +0100

    add initial kobo processor

--- src/bleanser/core/utils.py
+++ src/bleanser/core/utils.py
@@ -8,7 +8,6 @@ from typing import List
 def get_tables(c: Connection) -> List[str]:
     cur = c.execute('SELECT name FROM sqlite_master')
     names = [c[0] for c in cur]
-    names.remove('sqlite_sequence') # hmm not sure about it
     return names
 
 
