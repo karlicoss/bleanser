a670816f60515e86fc1b47d8883e311cda137eba
Dima Gerasimov <karlicoss@gmail.com>
2021-04-09 23:06:07 +0100

    firefox: clean up some volatile stuff in moz_places

--- src/bleanser/modules/firefox.py
+++ src/bleanser/modules/firefox.py
@@ -24,6 +24,8 @@ class Normaliser(SqliteNormaliser):
 
     def cleanup(self, c: Connection) -> None:
         tool = Tool(c)
+        tool.drop_index('moz_places_guid_uniqueindex')
+        [(visits_before,)] = c.execute('SELECT count(*) FROM moz_historyvisits')
         tool.drop_cols(
             table='moz_places',
             cols=[
@@ -32,19 +34,33 @@ class Normaliser(SqliteNormaliser):
                 'last_visit_date',
                 'visit_count',
                 # ugh... sometimes changes because of notifications, e.g. twitter/youtube?, or during page load
+                'hidden',
+                'typed',
                 'title',
                 'description',
                 'preview_image_url',
+
+                # ugh. sometimes changes for no reason...
+                # and anyway, for history the historyvisits table refers place_id (this table's actual id)
+                'guid',
+
+                'foreign_count', # jus some internal refcount thing... https://bugzilla.mozilla.org/show_bug.cgi?id=1017502
             ]
         )
         tool.drop_cols(
             table='moz_bookmarks',
             cols=['lastModified'],  # changing all the time for no reason?
+            # todo hmm dateAdded might change when e.g. firefox reinstalls and it adds default bookmarks
+            # probably not worth the trouble
         )
         tool.drop('moz_meta')
         tool.drop('moz_origins')  # prefix/host/frequency -- not interesting
         # todo not sure...
         tool.drop('moz_inputhistory')
+
+        # sanity check just in case... can remove after we get rid of triggers properly...
+        [(visits_after,)] = c.execute('SELECT count(*) FROM moz_historyvisits')
+        assert visits_before == visits_after, (visits_before, visits_after)
 
 
 if __name__ == '__main__':
