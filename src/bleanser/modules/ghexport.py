de03081c4cab5ac3fc5d800c307a56e0f0020f76
Dima Gerasimov <karlicoss@gmail.com>
2021-04-14 00:27:29 +0100

    initial ghexport module

---
+++ src/bleanser/modules/ghexport.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from bleanser.core.utils import Json
+from bleanser.core.json import JsonNormaliser, delkey
+
+
+class GhexportNormaliser(JsonNormaliser):
+    def cleanup(self, j: Json) -> None:
+        # TODO hmm... need a way to cleanup after it's been dumped to json with paths... ugh
+        # TODO ugh... for these various stats are always flaky (like number of starts) and I don't necessarily wanna track them...
+        del j['watched']
+        del j['starred']
+        # TODO crap. traffic changing all the time.. and I want to keep it
+
+
+if __name__ == '__main__':
+    from bleanser.core import main
+    main(Normaliser=GhexportNormaliser)
