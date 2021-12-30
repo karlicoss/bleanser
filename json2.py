bd377468edb775b60e756e64bb4540ba14f4fb26
Dima Gerasimov <karlicoss@gmail.com>
2021-12-30 13:09:02 +0000

    some wip on json handling; github events processing

--- json2.py
+++ json2.py
@@ -1,13 +1,61 @@
 #!/usr/bin/env python3
-import json
 from pathlib import Path
 import sys
 from subprocess import check_call
 
+import json
 
-def process(fo) -> None:
-    j = json.load(fo)
+# todo hmm, seems that ther isn't that much perf difference, at least on hyperfine
+# maybe double check later
+# import orjson as json
+
+
+# TODO warn about some data being cleaned, refer to the sources
+def pp_github(j):
+    # todo later compare to jq somehow? but doubt it'd be faster
+    from itertools import chain
+
+    # TODO hmm
+    # what should we do with repos :::: clones thing?
+    # maybe we could check domination relationship in a more clever way somehow?...
+    # e.g. here clones -> count ???
+    # ': 0, 'uniques': 0, 'views': []}, 'clones': {'count': 29, 'uniques': 14, 'clones': [{'timestamp': '2021-11-29T00:00:00Z'|       'pull': True}, 'traffic': {'views': {'count': 0, 'uniques': 0, 'views': []}, 'clones': {'count': 27, 'uniques': 13, 'cl
+    #   , 'count': 3, 'uniques': 2}, {'timestamp': '2021-11-30T00:00:00Z', 'count': 2, 'uniques': 1}, {'timestamp': '2021-12-01T|       ones': [{'timestamp': '2021-11-29T00:00:00Z', 'count': 1, 'uniques': 1}, {'timestamp': '2021-11-30T00:00:00Z', 'count':
+
+    # TODO not sure what to do with it...
+    # for x in j['repos']:
+    #     del x['traffic']
+
+    for x in chain(j['watched'], j['starred']):
+        for key in [
+                'watchers', 'stargazers_count', 'watchers_count',
+
+                # updated_at -- seems that it's updated every time there is a star etc...
+                'updated_at',
+                'forks', 'forks_count',
+
+                'open_issues', 'open_issues_count',
+
+                # eh, not sure about these...
+                'pushed_at',
+                'size',
+        ]:
+            del x[key]
+
+
+def preprocess(*, j, name):
+    # todo not sure how defensive should be?
+
+    # todo not sure if there is a better way
+    if '/github-events/' in name:
+        pp_github(j)
+
+
+def process(fo, *, name) -> None:
+    j = json.loads(fo.read())
     # todo would be nice to close it here
+
+    preprocess(j=j, name=name)
 
     if isinstance(j, list):
         res = {'<toplevel>': j} # meh
@@ -37,14 +85,17 @@ def main() -> None:
     p1 = args.path1
     p2 = args.path2
 
+    # TODO compare performance fo handling compressed and uncompressed files
+    from bleanser.core.kompress import CPath
+
     if p2 is None:
         # handle single file
         if p1 == '-':
             process(fo=sys.stdin)
         else:
-            path = Path(p1).absolute()
-            with path.open('r') as fo:
-                process(fo=fo)
+            path = str(Path(p1).absolute())
+            with CPath(path).open() as fo:
+                process(fo=fo, name=path)
     else:
         assert p1 != '-' and p2 != '-'
         # hacky way to compare
@@ -53,10 +104,14 @@ def main() -> None:
                 cat = 'xzcat'
             else:
                 cat = 'cat'
-            return f'{cat} {p} | {__file__} - | sort'
+            # {cat} {p} | {__file__} -
+            return f'{__file__} {p} | sort'
         c1 = cc(p1)
         c2 = cc(p2)
-        cmd = f'vimdiff -c "windo set wrap" <({c1}) <({c2})'
+        # wrap = ' -c "windo set wrap" '#  -- eh, not super convenient?
+        wrap = ''
+        # FIXME pipefail? doesn't work well..
+        cmd = f'vimdiff {wrap} <({c1}) <({c2})'
         check_call(cmd, shell=True, executable='/bin/bash')
 
 
