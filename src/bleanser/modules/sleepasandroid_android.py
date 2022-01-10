dc9940a705f441614036e426d4c81855f7287d09
Dima Gerasimov <karlicoss@gmail.com>
2022-01-10 00:31:37 +0000

    add sleepasandroid module

---
+++ src/bleanser/modules/sleepasandroid_android.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from bleanser.core.sqlite import SqliteNormaliser, Tool
+from bleanser.core.utils import get_tables
+
+
+class Normaliser(SqliteNormaliser):
+    MULTIWAY = False  # could use it, but no need really?
+    DELETE_DOMINATED = True
+
+
+    def check(self, c) -> None:
+        tables = Tool(c).get_schemas()
+        assert 'noise'   in tables, tables
+        assert 'records' in tables, tables
+
+
+    def cleanup(self, c) -> None:
+        self.check(c)
+
+        # if not finished it's gonna constantly change
+        res = c.execute(f'DELETE FROM records WHERE finished = 0')
+        assert res.rowcount <= 1, res.rowcount
+
+
+if __name__ == '__main__':
+    Normaliser.main()
