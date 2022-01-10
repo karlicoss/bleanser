6224825de19305a95152ee438b317d7e2fcad8b6
Dima Gerasimov <karlicoss@gmail.com>
2022-01-10 21:19:26 +0000

    bluemaestro: handle empty table, add test

--- src/bleanser/modules/bluemaestro.py
+++ src/bleanser/modules/bluemaestro.py
@@ -26,7 +26,11 @@ class Normaliser(SqliteNormaliser):
 
             ## get rid of downloadUnix -- it's changing after export and redundant info
             [[ut]] = list(c.execute(f'SELECT downloadUnix FROM {device}_info'))
-            last_log = max(t for t in tables if t.endswith('log'))
+            last_logs = [t for t in tables if t.endswith('log')]
+            if len(last_logs) == 0:
+                # seems like no data yet
+                return
+            last_log = max(last_logs)
             assert last_log == f'{device}_{ut}_log', last_log
             tool.drop_cols(table=f'{device}_info', cols=['downloadUnix'])
 
@@ -35,8 +39,31 @@ if __name__ == '__main__':
     Normaliser.main()
 
 
-# TODO add tests
-# think I've had jdoe or something with example databases..
-# BM1 = thedb(bm / '20210225015750')
-# BM2 = thedb(bm / '20210228191259')
-# BM2 strictly dominates BM1
+# TODO think I've had jdoe or something with example databases..
+def test_bluemaestro() -> None:
+    from bleanser.tests.common import TESTDATA, actions2
+    res = actions2(path=TESTDATA / 'bluemaestro', rglob='*.db*', Normaliser=Normaliser)
+
+    assert res.remaining == [
+        '20180720.db',
+        # '20180724.db',  # move
+        '20180728.db',
+        # '20180730.db',  # move
+        '20180731.db',
+
+        '20190723100032.db', # keep, everything changed
+        # TODO need to investigate, some values have changed a bit, like 1st digit after decimal point
+        # even timestmaps chagned sometimes (e.g. just last second)
+        # hpi bluemaestro module has something for handling this, I think
+        '20190724101707.db',
+        # same as above
+        '20190727104723.db',
+
+        '20200208225936.db', # keep, everything changed (several months diff)
+        # '20201209083427/bmgateway.db',  # move, completely dominated by the next
+        # '20210131102917/bmgateway.db',  # move, completely dominated by the next
+        # '20210207183947/bmgateway.db',  # move, completely dominated by the next
+        '20210216211844/bmgateway.db',  # keep, errored because couldn't find last _log item
+        '20211103234924/bmgateway.db',  # same, previous errored
+        '20211106191208/bmgateway.db',
+    ]
