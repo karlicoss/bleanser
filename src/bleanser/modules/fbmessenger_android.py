7497b98a3187f7fa75ce88002d858ff8699ac789
Dima Gerasimov <karlicoss@gmail.com>
2022-01-23 14:36:28 +0000

    fbmessenger: process threads_db2

--- src/bleanser/modules/fbmessenger_android.py
+++ src/bleanser/modules/fbmessenger_android.py
@@ -3,12 +3,11 @@ from bleanser.core.sqlite import SqliteNormaliser, Tool
 
 
 class Normaliser(SqliteNormaliser):
-    # TODO
-    # MULTIWAY = True
-    # PRUNE_DOMINATED = True
+    MULTIWAY = True
+    PRUNE_DOMINATED = True
 
-    # msys_database
     ALLOWED_BLOBS = {
+        # msys_database
           ('secure_message_pre_keys_v2'           , '*') # not useful
         ## mostly empty, dunno what it is
         , ('secure_message_client_identity_v2'    , '*')
@@ -50,21 +49,55 @@ class Normaliser(SqliteNormaliser):
     DROP_VIRTUAL_TABLES = True
 
     def check(self, c) -> None:
-        return
         tables = Tool(c).get_tables()
-        assert 'Feeds' in tables, tables
-        eps = tables['FeedItems']
-        assert 'link' in eps
-        assert 'read' in eps
 
-        # should be safe to use multiway because of these vvv
-        media = tables['FeedMedia']
-        assert 'played_duration'  in media
-        assert 'last_played_time' in media
-
+        mtable = tables['messages']
+        assert 'text'         in mtable, mtable
+        assert 'timestamp_ms' in mtable, mtable
 
     def cleanup(self, c) -> None:
+        t = Tool(c)
+        if 'logging_events_v2' in t.get_tables():
+            self.cleanup_msys_database(c)
+            return
+
         self.check(c)
+        t.drop('properties')  # eh, just some weird random key-values
+        t.drop('virtual_folders')  # looks like some temporary thing, just a few values
+
+        for name in ['folders', 'threads', 'thread_participants', 'thread_themes']:
+            t.drop_cols(name, cols=['_id'])  # meh.. sometimes changes for no reason
+
+        t.drop_cols('folders', cols=['timestamp_ms'])  # changes all the time
+        t.drop_cols('thread_participants', cols=[
+            'last_read_receipt_time',
+            'last_read_receipt_watermark_time',
+            'last_delivered_receipt_time',
+        ])
+        # changes all the time
+        t.drop_cols('threads', cols=[
+            'sequence_id',
+            'last_snippet_update_timestamp_ms',
+            'last_message_timestamp_ms',
+            'last_message_id',
+            'last_fetch_time_ms',
+            'last_read_timestamp_ms',
+            'timestamp_ms',
+            'snippet',
+            'admin_snippet',
+            'approx_total_message_count',
+            'unread_message_count',
+            'vanish_mode_selection_timestamp',
+
+            # todo?
+            'snippet_sender',
+            'senders',
+        ])
+
+
+    def cleanup_msys_database(self, c) -> None:
+        # TODO eh... tbh, not sure, msys database only contains some contacts and bunch of cryptic data?
+        # self.check(c) # TODO
 
         t = Tool(c)
         t.drop('secure_message_server_time_v2')
