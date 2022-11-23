373efbef6a2f762c4e741c1d4578536350b5c5f5
karlicoss <karlicoss@gmail.com>
2022-11-23 23:49:38 +0000

    whatsapp: cleanup tables/columns
    pruning runs:
    - 9906 Mb /14263 Mb , 1037 /1460 files
    - 3194 Mb /4356 Mb  , 288  /423 files
    
    seems pretty effective perhaps because messages table seems cumulative? didn't expect that

--- src/bleanser/modules/whatsapp_android.py
+++ src/bleanser/modules/whatsapp_android.py
@@ -47,33 +47,147 @@ class Normaliser(SqliteNormaliser):
         ('message_add_on_orphan', 'orphan_message_data'),
         ('payment_background', 'media_key'),
         ('message_broadcast_ephemeral', 'shared_secret'),
+        ('message_secret', 'message_secret'),
+        ('message_poll', 'enc_key'),
+
 
         # the only interesting ones perhaps? checked manually and it's dumped as hex or something, so should be good
         ('audio_data', 'waveform'),
         ('messages', 'raw_data'),  # this one is mostly NULL except one row??
     }
 
-    #def check(self, c) -> None:
-    #    tables = Tool(c).get_tables()
-    #    assert 'Feeds' in tables, tables
-    #    eps = tables['FeedItems']
-    #    assert 'link' in eps
-    #    assert 'read' in eps
+    def check(self, c) -> None:
+        tables = Tool(c).get_tables()
+        chat = tables['chat']
+        assert 'subject' in chat
+        assert 'created_timestamp' in chat 
 
-    #    # should be safe to use multiway because of these vvv
-    #    media = tables['FeedMedia']
-    #    assert 'played_duration'  in media
-    #    assert 'last_played_time' in media
+        if 'messages' in tables:
+            msgs = tables['messages']
+            assert 'data' in msgs
+        else:
+            msgs = tables['message'] # new format (at least as of Nov 2022)
+            assert 'text_data' in msgs
 
+        assert 'timestamp' in msgs
 
-    #def cleanup(self, c) -> None:
-    #    self.check(c)
+        # maybe also
+        # audio_data
+        # call_log (call_id, timestamp)
+        # chat
 
-    #    t = Tool(c)
-    #    # often changing, no point keeping
-    #    t.drop_cols(table='Feeds', cols=[
-    #        'last_update',
-    #    ])
+        # TODO group_participant_user
+        # TODO message_media not sure if useful?
+
+    def cleanup(self, c) -> None:
+        self.check(c)
+
+        t = Tool(c)
+
+        # note: there are WAY more useless tables there, but for the most part they are empty
+        for table in [
+            'frequent',
+            'frequents', # freq used contacts
+            'group_notification_version', 
+            'group_participant_device', # not sure who'd need it
+            'media_hash_thumbnail',
+            'media_refs', # just random file paths with counters
+            'message_forwarded', # keeps track of some forward_score??
+
+            ## some sort of search index
+            'message_ftsv2',
+            'message_ftsv2_content',
+            'message_ftsv2_docsize',
+            'message_ftsv2_segdir',
+            'message_ftsv2_segments',
+            'message_ftsv2_stat',
+            ##
+
+            'message_streaming_sidecar', #  some random numbers
+            'message_thumbnail',
+            'message_thumbnails',
+            'message_ui_elements',
+            'message_ui_elements_reply',
+            'message_vcard',
+            'message_vcard_jid',
+            'message_view_once_media',
+            'messages',
+
+            ## also some search index
+            'messages_fts',
+            'messages_fts_content',
+            'messages_fts_segdir',
+            'messages_fts_segments',
+            ## 
+
+            'messages_links',
+            'messages_quotes',
+            'messages_vcards',
+            'messages_vcards_jids',
+            'mms_thumbnail_metadata',
+            'primary_device_version', # just some random numbers??
+            'props', # some random metdata, changes all the time
+            'receipt_device',
+            'receipt_orphaned',
+            'receipt_user',
+            'receipts', # not sure why would it be useful to keep track of
+            'status', # keeps track of last read msg or something
+            'user_device',
+            'user_device_info',
+
+            ### from newer app versions
+            'message_details', # some crap like author_device_jid
+            ## some sort of aggregates or indices into stuff like emoji? wtf...
+            'message_add_on',
+            'message_add_on_reaction',
+            'message_add_on_receipt_device',
+            ##
+
+            'message_system', # random numbers
+            'message_system_chat_participant',
+        ]:
+            t.drop(table)
+
+        t.drop_cols(table='chat', cols=[
+	        'display_message_row_id',
+            'last_message_row_id',
+            'last_read_message_row_id',
+            'last_read_receipt_sent_message_row_id',
+            'last_important_message_row_id',
+            'sort_timestamp',
+            'unseen_earliest_message_received_time',
+            'unseen_message_count',
+            'unseen_row_count',
+            'unseen_message_reaction_count',
+
+            ## newer db versions
+            # flaky fields
+            'last_read_message_sort_id',
+            'display_message_sort_id',
+            'last_message_sort_id',
+            'last_read_receipt_sent_message_sort_id',
+            'last_message_reaction_row_id',
+            'last_seen_message_reaction_row_id',
+            'mod_tag',
+
+            # TODO ugh. created_timestamp might be flaky? seen it goign from NULL to actual value??
+        ])
+
+        t.drop_cols(table='message', cols=[
+            ## flaky, no idea what is it
+            'origination_flags',
+            'message_add_on_flags',
+            'status',
+            ##
+        ])
+
+        t.drop_cols(table='message_media', cols=[
+            'has_streaming_sidecar', # flaky 0/1
+            'original_file_hash', # ??? sometimes goes from value to NULL
+
+            # todo media_name might be flaky?? sometimes sets from NULL to file name?
+            # same infor is in file_path though... so idk
+        ])
 
 
 if __name__ == '__main__':
