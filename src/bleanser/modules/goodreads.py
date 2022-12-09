38dd69bfec0809bd6919c7e4341212ca4cd80c0d
karlicoss <karlicoss@gmail.com>
2022-12-09 00:15:48 +0000

    module for goodreads

---
+++ src/bleanser/modules/goodreads.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from bleanser.modules.xml_clean import Normaliser as XmlNormaliser
+
+
+class Normaliser(XmlNormaliser):
+    MULTIWAY = True
+    PRUNE_DOMINATED = True
+
+    def cleanup(self, t):
+        for key in [
+                'average_rating',
+                'text_reviews_count',
+                'ratings_count',
+                'book/description',  # volatile
+        ]:
+            for x in t.findall('.//' + key):
+                x.getparent().remove(x)
+        return t
+
+
+if __name__ == '__main__':
+    Normaliser.main()
