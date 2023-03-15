bb9bf6091b44f401b0ee253ebeb405b7aba6fae7
karlicoss <karlicoss@gmail.com>
2023-03-15 19:23:43 +0000

    update whatsapp

--- src/bleanser/modules/whatsapp_android.py
+++ src/bleanser/modules/whatsapp_android.py
@@ -50,6 +50,7 @@ class Normaliser(SqliteNormaliser):
         ('message_broadcast_ephemeral', 'shared_secret'),
         ('message_secret', 'message_secret'),
         ('message_poll', 'enc_key'),
+        ('suggested_replies', 'customer_message_embedding'),
 
 
         # the only interesting ones perhaps? checked manually and it's dumped as hex or something, so should be good
