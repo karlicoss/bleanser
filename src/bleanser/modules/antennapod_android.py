9b3e348f0071495a4e971189c755476a97a3a168
karlicoss <karlicoss@gmail.com>
2023-03-28 21:22:54 +0100

    antennapod_android: better pruning

--- src/bleanser/modules/antennapod_android.py
+++ src/bleanser/modules/antennapod_android.py
@@ -27,7 +27,27 @@ class Normaliser(SqliteNormaliser):
         # often changing, no point keeping
         t.drop_cols(table='Feeds', cols=[
             'last_update',
+            'last_update_failed',
+            'image_url',  # volatile
+            'minimal_duration_filter',
         ])
+
+        t.drop_cols(table='FeedMedia', cols=[
+            'download_url',  # sometimes change, especially tracking links -- probs not worth keeping anyway
+
+            'filesize',  # no idea why would it change, but it does sometimes
+        ])
+
+        t.drop_cols(table='FeedItems', cols=[
+            'title',  # useful feed, but volatile so best to ignore
+
+            'content_encoded',  # no idea what is it but volatile
+
+            'description',  # often changing, no need to keep
+            'image_url',  # volatile
+        ])
+
+        t.drop('Queue')
 
 
 if __name__ == '__main__':
