844840561371eeb3b5c2d3d477dc2acb866d26b2
karlicoss <karlicoss@gmail.com>
2023-08-25 00:14:57 +0100

    add intial module for tiktok android app

---
+++ src/bleanser/modules/tiktok_android.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from bleanser.core.sqlite import SqliteNormaliser, Tool
+
+
+class Normaliser(SqliteNormaliser):
+    MULTIWAY = True
+    PRUNE_DOMINATED = True
+
+    ALLOWED_BLOBS = {
+        ('msg', 'content_pb'),
+        ('im_search_index_official_segments', '*'),
+        ('im_search_index_official_segdir', '*'),
+        ('im_search_index_official_docsize', '*'),
+        ('im_search_index_official_stat', '*'),
+    }
+
+    def check(self, c) -> None:
+        tables = Tool(c).get_tables()
+
+        messages = tables['msg']
+        assert 'msg_uuid' in messages
+        assert 'content' in messages
+
+    def cleanup(self, c) -> None:
+        self.check(c)
+
+
+if __name__ == '__main__':
+    Normaliser.main()
