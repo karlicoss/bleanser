7def87a55b07c5568464f7d7f5241d68b4d9884c
karlicoss <karlicoss@gmail.com>
2023-09-03 02:40:43 +0100

    ghexport: implement proper cleanup, flatten repo traffic to make it possible to diff

--- src/bleanser/modules/ghexport.py
+++ src/bleanser/modules/ghexport.py
@@ -3,12 +3,51 @@ from bleanser.modules.json_new import JsonNormaliser, Json
 
 
 class Normaliser(JsonNormaliser):
-    def cleanup(self, j: Json) -> None:
-        # TODO hmm... need a way to cleanup after it's been dumped to json with paths... ugh
-        # TODO ugh... for these various stats are always flaky (like number of starts) and I don't necessarily wanna track them...
-        del j['watched']
-        del j['starred']
-        # TODO crap. traffic changing all the time.. and I want to keep it
+    PRUNE_DOMINATED = True
+    MULTIWAY = True
+
+    def cleanup(self, j: Json) -> Json:
+        if isinstance(j, list):
+            # old format -- I think only contained events log or something
+            return j
+
+        for what in ['watched', 'starred', 'subscriptions']:
+            for r in j[what]:
+                # these are gonna be super flaky, so just ignore from diff
+                for k in [
+                        'stargazers_count',
+                        'watchers',
+                        'watchers_count',
+                        'updated_at',
+                        'pushed_at',
+                        'forks',
+                        'forks_count',
+                        'open_issues',
+                        'open_issues_count',
+                        'size',
+                ]:
+                    r.pop(k, None)
+
+        for r in j['repos']:
+            ## need to 'flatten' traffic, otherwise it can't properly figure out diffs
+            ## TODO possible to make generic, e.g. hint the normaliser that we need to flatten .repos.traffic.clones field
+            traffic = r.get('traffic')
+            if traffic is None:
+                continue
+            for key in ['clones', 'views']:
+                xxx = traffic[key]
+                xxx.pop('count')   # aggregate
+                xxx.pop('uniques') # aggregate
+                assert xxx.keys() == {key}
+                # NOTE: we ignore first and last traffic entry since timestamps are aligned to the closest day
+                # so they are always going to be kinda flaky
+                for c in xxx[key][1: -1]:
+                    ts = c['timestamp']
+                    j[f'{r["full_name"]}_traffic_{key}_{ts}'] = c
+                xxx.pop(key)
+
+        # TODO should probably prefer in place cleanup to make consistent with sqlite? not sure
+        return j
 
 
 if __name__ == '__main__':
