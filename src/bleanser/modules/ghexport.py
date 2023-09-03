b314c28bd079da925780071707e5bfd0e9b0e3cd
karlicoss <karlicoss@gmail.com>
2023-09-03 03:29:48 +0100

    ghexport: improve cleanup and sort out some volatile keys

--- src/bleanser/modules/ghexport.py
+++ src/bleanser/modules/ghexport.py
@@ -11,24 +11,36 @@ class Normaliser(JsonNormaliser):
             # old format -- I think only contained events log or something
             return j
 
+        volatile = [
+            'stargazers_count',
+            'watchers',
+            'watchers_count',
+            'forks',
+            'forks_count',
+            'open_issues',
+            'open_issues_count',
+        ]
+
         for what in ['watched', 'starred', 'subscriptions']:
             for r in j[what]:
                 # these are gonna be super flaky, so just ignore from diff
                 for k in [
-                        'stargazers_count',
-                        'watchers',
-                        'watchers_count',
+                        *volatile,
                         'updated_at',
                         'pushed_at',
-                        'forks',
-                        'forks_count',
-                        'open_issues',
-                        'open_issues_count',
                         'size',
                 ]:
                     r.pop(k, None)
 
         for r in j['repos']:
+            repo_name = r["full_name"]
+
+            for k in volatile:
+                v = r.get(k)
+                if v is None:
+                    continue
+                r[k] = r[k] // 10 * 10
+
             ## need to 'flatten' traffic, otherwise it can't properly figure out diffs
             ## TODO possible to make generic, e.g. hint the normaliser that we need to flatten .repos.traffic.clones field
             traffic = r.get('traffic')
@@ -43,8 +55,12 @@ class Normaliser(JsonNormaliser):
                 # so they are always going to be kinda flaky
                 for c in xxx[key][1: -1]:
                     ts = c['timestamp']
-                    j[f'{r["full_name"]}_traffic_{key}_{ts}'] = c
+                    j[f'{repo_name}_traffic_{key}_{ts}'] = c
                 xxx.pop(key)
+            for key in ['popular/paths', 'popular/referrers']:
+                # TODO hmm these are still quite flaky? they collect stats over last two weeks so can change a lot..
+                j[f'{repo_name}_traffic_{key}'] = traffic[key]
+                traffic.pop(key)
 
         # TODO should probably prefer in place cleanup to make consistent with sqlite? not sure
         return j
