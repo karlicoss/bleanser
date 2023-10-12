9a2c1b3830c654e2ffd130e1430438eb25a245df
Sean Breckenridge <seanbrecke@gmail.com>
2023-10-12 16:52:00 -0700

    add redirect files for old modules

---
+++ src/bleanser/core/sqlite.py
@@ -0,0 +1,8 @@
+from bleanser.core.modules.sqlite import *
+
+import warnings
+
+warnings.warn(
+    "Module 'bleanser.core.sqlite' is deprecated. Use 'bleanser.core.modules.sqlite' instead.",
+    DeprecationWarning,
+)
---
+++ src/bleanser/modules/json_new.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from bleanser.core.modules.json_new import *  # noqa: F403, F401
+
+import warnings
+warnings.warn("Module bleanser.modules.json_new is deprecated. Use bleanser.core.modules.json_new instead.", DeprecationWarning)
