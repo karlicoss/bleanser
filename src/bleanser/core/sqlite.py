867a7a58f83ae91f3703a9f4d0d554936644a90f
karlicoss <karlicoss@gmail.com>
2023-10-13 18:44:11 +0100

    add main call to deprecated legacy modules, otherwise they won't do anything

--- src/bleanser/core/sqlite.py
+++ src/bleanser/core/sqlite.py
@@ -6,3 +6,7 @@ warnings.warn(
     "Module 'bleanser.core.sqlite' is deprecated. Use 'bleanser.core.modules.sqlite' instead.",
     DeprecationWarning,
 )
+
+
+if __name__ == '__main__':
+    SqliteNormaliser.main()  # noqa: F405
--- src/bleanser/modules/json_new.py
+++ src/bleanser/modules/json_new.py
@@ -3,3 +3,7 @@ from bleanser.core.modules.json_new import *  # noqa: F403, F401
 
 import warnings
 warnings.warn("Module 'bleanser.modules.json_new' is deprecated. Use 'bleanser.core.modules.json_new' instead.", DeprecationWarning)
+
+
+if __name__ == '__main__':
+    JsonNormaliser.main()  # noqa: F405
