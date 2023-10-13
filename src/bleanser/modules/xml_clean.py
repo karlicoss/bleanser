4388c155435a36a741d739e4da2d219eb956e415
karlicoss <karlicoss@gmail.com>
2023-10-13 18:44:11 +0100

    add main call to deprecated legacy modules, otherwise they won't do anything

--- src/bleanser/modules/xml_clean.py
+++ src/bleanser/modules/xml_clean.py
@@ -2,3 +2,7 @@ from bleanser.core.modules.xml_clean import *  # noqa: F401, F403
 
 import warnings
 warnings.warn("Module 'bleanser.modules.xml_clean' is deprecated. Use 'bleanser.core.modules.xml_clean' instead.", DeprecationWarning)
+
+
+if __name__ == '__main__':
+    Normaliser.main()  # noqa: F405
