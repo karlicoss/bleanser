50a8a121eda77730f8e7bce8e969cfdd0c0b0200
karlicoss <karlicoss@gmail.com>
2023-10-17 23:31:24 +0100

    kobo

--- src/bleanser/modules/hpi/kobo.py
+++ src/bleanser/modules/hpi/kobo.py
@@ -4,27 +4,29 @@ from typing import Any, Iterator
 from bleanser.core.modules.extract import ExtractObjectsNormaliser
 
 from my.core.cfg import tmp_config
-import my.hypothesis
-
-
-# FIXME need to disable cachew when using normalising via HPI
-# otherwise will mess up the cache all the time
-# or even potentially can give inconsistent results if there is a bug in cache key
+import my.kobo
 
 
 class Normaliser(ExtractObjectsNormaliser):
     def extract_objects(self, path: Path) -> Iterator[Any]:
         class config:
-            class hypothesis:
+            class kobo:
                 export_path = path
 
-        with tmp_config(modules=my.hypothesis.__name__, config=config):
+        with tmp_config(modules=my.kobo.__name__, config=config):
+            assert len(my.kobo.DATABASES) == 1
+            yield from []
+
+            yield from my.kobo._iter_highlights()
+            # iter_highlights
+            # iter_events
+            #
             ## sanity check to make sure tmp_config worked as expected
             # for most modules should be able to use module.inputs() directly though
-            dal = my.hypothesis._dal()
-            assert len(dal.sources) == 1
+            # dal = my.hypothesis._dal()
+            # assert len(dal.sources) == 1
             ##
-            yield from my.hypothesis.highlights()
+            # yield from my.hypothesis.highlights()
 
 
 if __name__ == '__main__':
