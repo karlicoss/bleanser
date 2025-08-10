a3f6f1defb7dd1746d681c25c877dcaaf2ecfda0
Dima Gerasimov <karlicoss@gmail.com>
2025-08-10 21:27:23 +0100

    cli: add main when running pythonc -m bleanser to suggest running a specific module

--- src/bleanser/core/__main__.py
+++ src/bleanser/core/__main__.py
@@ -1,12 +1,18 @@
-# TODO hmm so we kind of need a specific Normaliser for bleanser, so calling
-# python3 -m bleanser.core (or just -m bleanser) doesn't make much sense
-# it could probs take in module name, and then call it? like python3 -m bleanser modules.xxx
-# but it's the same as calling python -m bleanser.modules.xxx
-# TODO maybe this thing could do module discovery or something?
+import sys
+
+
 def main() -> None:
-    pass
+    print(
+        """
+This command is a NO-OP.
+You should run bleanser via a specific module, e.g:
+    python3 -m bleanser.modules.pocket ...
+""",
+        file=sys.stderr,
+    )
+    # TODO maybe this thing could do module discovery or something?
+    sys.exit(1)
 
 
 if __name__ == '__main__':
-    # FIXME warn if we're running this command? kinda confusing otherwise
     main()
