ce1c6f27cd4ce53e91062c41546cbf16b085d9d1
Dima Gerasimov <karlicoss@gmail.com>
2025-12-26 17:23:29 +0000

    tests: move sqlite tests to tests/sqlite, makes the actual source cleaner
    also allows to get rid of pytest.ini hack to avoid duplicate tests

--- src/bleanser/core/sqlite.py
+++ src/bleanser/core/sqlite.py
@@ -1,13 +1,15 @@
 import warnings
 
-from bleanser.core.modules.sqlite import *
-
 warnings.warn(
     "Module 'bleanser.core.sqlite' is deprecated. Use 'bleanser.core.modules.sqlite' instead.",
     DeprecationWarning,
     stacklevel=2,
 )
 
+from typing import TYPE_CHECKING
 
-if __name__ == '__main__':
-    SqliteNormaliser.main()  # noqa: F405
+if not TYPE_CHECKING:
+    from bleanser.core.modules.sqlite import *
+
+    if __name__ == '__main__':
+        SqliteNormaliser.main()  # noqa: F405
