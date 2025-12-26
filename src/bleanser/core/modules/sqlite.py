665954749c0aa1b792a5e4e3bb10455b4d88885d
Dima Gerasimov <karlicoss@gmail.com>
2025-12-26 17:23:29 +0000

    tests: move sqlite tests to tests/sqlite, makes the actual source cleaner
    also allows to get rid of pytest.ini hack to avoid duplicate tests

--- src/bleanser/core/modules/sqlite.py
+++ src/bleanser/core/modules/sqlite.py
@@ -11,16 +11,12 @@ from collections.abc import Iterator, Sequence
 from contextlib import contextmanager
 from pathlib import Path
 from sqlite3 import Connection
-from typing import Any
 
 from plumbum import local  # type: ignore[import-untyped]
 
-from ..common import Keep, Prune, parametrize
 from ..processor import (
     BaseNormaliser,
     Normalised,
-    compute_groups,
-    compute_instructions,
     sort_file,
     unique_file_in_tempdir,
 )
@@ -109,154 +105,6 @@ def checked_db(db: Path, *, allowed_blobs: AllowedBlobs | None) -> Path:
 
 grep = local['grep']
 sqlite_cmd = local['sqlite3']
-
-
-def _dict2db(d: dict, *, to: Path) -> Path:
-    with sqlite3.connect(to) as conn:
-        for table_name, rows in d.items():
-            schema = rows[0]
-            s = ', '.join(schema)
-            qq = ', '.join('?' for _ in schema)
-            conn.execute(f'CREATE TABLE `{table_name}` ({s})')
-            conn.executemany(f'INSERT INTO `{table_name}` VALUES ({qq})', rows[1:])
-    conn.close()
-    return to  # just for convenience
-
-
-def test_sqlite_simple(tmp_path: Path) -> None:
-    class TestNormaliser(SqliteNormaliser):
-        MULTIWAY = False
-        PRUNE_DOMINATED = True
-
-    func = lambda paths: compute_groups(paths, Normaliser=TestNormaliser)
-
-    d: dict[str, Any] = {'tq': [['col1', 'col2']]}
-    ### just one file
-    db1 = _dict2db(d, to=tmp_path / '1.db')
-    [g11] = func([db1])
-    assert g11.items  == [db1]  # fmt: skip
-    assert g11.pivots == [db1]
-    ###
-
-    ### simple 'dominates' test
-    d['t1'] = [
-        ['col1', 'col2'],
-        [1     , 2     ],
-        [3     , 4     ],
-    ]  # fmt: skip
-    db2 = _dict2db(d, to=tmp_path / '2.db')
-
-    [g21] = func([db1, db2])
-    assert g21.items  == [db1, db2]  # fmt: skip
-    assert g21.pivots == [db1, db2]
-    ###
-
-    ### test error handling
-    db3 = tmp_path / '3.db'
-    db3.write_text('BAD')
-
-    # TODO wow it's really messy...
-    # fmt: off
-    [x1, x2] = func([db3, db1, db2])
-    assert x1.items  == [db3]
-    assert x1.pivots == [db3]
-    assert x2.items  == [db1, db2]
-    assert x2.pivots == [db1, db2]
-
-    [g31, g32, g33] = func([db1, db2, db3])
-    assert g31 == g21
-    assert g32.items  == [db2]
-    assert g32.pivots == [db2]
-    assert g33.items  == [db3]
-    assert g33.pivots == [db3]
-    # fmt: on
-    # FIXME check error reason
-    ###
-
-    ### test 'same' handling
-    db4 = _dict2db(d, to=tmp_path / '4.db')
-    db5 = _dict2db(d, to=tmp_path / '5.db')
-    db6 = _dict2db(d, to=tmp_path / '6.db')
-
-    dbs = [db1, db2, db3, db4, db5, db6]
-    [_g41, _g42, g43, g44] = func(dbs)
-    assert g43 == g33
-    assert g44.items  == [db4, db5, db6]  # fmt: skip
-    assert g44.pivots == [db4,      db6]  # fmt: skip
-    ###
-
-    instrs = compute_instructions(
-        dbs,
-        Normaliser=SqliteNormaliser,
-        threads=None,
-    )
-    assert list(map(type, instrs)) == [
-        Keep,   # 1
-        Keep,   # 2
-        Keep,   # 3
-        Keep,   # 4, keep the boundary
-        Prune,  # 5
-        Keep,   # 6, keep the boundary
-    ]  # fmt: skip
-
-    ### test when stuff was removed
-    del d['t1'][-1]
-    db7 = _dict2db(d, to=tmp_path / '7.db')
-    dbs = [db1, db2, db3, db4, db5, db6, db7]
-    [_, _, _, g54, g55, g56] = func(dbs)
-    assert g54 == g44
-    # TODO ugh. this is confusing... why it emits more pivots?
-    assert g55.items  == [db6]  # fmt: skip
-    assert g55.pivots == [db6]
-    assert g56.items  == [db7]  # fmt: skip
-    assert g56.pivots == [db7]
-    ###
-
-    instrs = compute_instructions(
-        dbs,
-        Normaliser=SqliteNormaliser,
-        threads=None,
-    )
-    assert list(map(type, instrs)) == [
-        Keep,   # 1
-        Keep,   # 2
-        Keep,   # 3
-        Keep,   # 4, keep the boundary
-        Prune,  # 5
-        Keep,   # 6, keep the boundary
-        Keep,   # 7,
-    ]  # fmt: skip
-
-
-@parametrize('multiway', [False, True])
-def test_sqlite_many(*, tmp_path: Path, multiway: bool) -> None:
-    class TestNormaliser(SqliteNormaliser):
-        MULTIWAY = multiway
-        PRUNE_DOMINATED = True
-
-    N = 2000
-
-    paths = []
-    d: dict[str, Any] = {}
-    for i in range(N):
-        if i % 10 == 0:
-            # flush so sometimes it emits groups
-            d = {'t': [('number',)]}
-        d['t'].append((i,))
-        p = _dict2db(d, to=tmp_path / f'{i:04}.db')
-        paths.append(p)
-
-    # shouldn't crash
-    _instructions = list(
-        compute_instructions(
-            paths,
-            Normaliser=TestNormaliser,
-            threads=None,
-        )
-    )
-
-
-# TODO add some tests for my own dbs? e.g. stashed
 
 
 class SqliteNormaliser(BaseNormaliser):
